5069eebc59397cb382af1ca1bec7fc165a1c5e16
Andrgoit <stakgrom@gmail.com>
2023-05-30 22:38:47 +0300

    add pages

---
+++ src/pages/LiecensesPage/LiecensesPage.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function LiecensesPage() {
+  return <div>LiecensesPage</div>;
+}
---
+++ src/pages/ProtectionPage/ProtectionPage.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ProtectionPage() {
+  return <div>ProtectionPage</div>;
+}
