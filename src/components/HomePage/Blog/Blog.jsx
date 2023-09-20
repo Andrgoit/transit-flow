5b9a46a4a2a98c227f974487f37056e23cdb1b21
Andrgoit <stakgrom@gmail.com>
2023-09-21 00:59:47 +0300

    refactoring blog section

--- src/components/HomePage/Blog/Blog.jsx
+++ src/components/HomePage/Blog/Blog.jsx
@@ -1,22 +1,5 @@
-import {
-  StyledSection,
-  StyledContainer,
-  StyledTtileSection,
-  StyledEventsList,
-  StyledEventItem,
-  StyledImgContainer,
-  StyledDateContainer,
-  StyledIconContainer,
-  StyledDayText,
-  StyledMonthText,
-  StyledTextContainer,
-  StyledTitle,
-  StyledText,
-  StyledTextListContainer,
-  StyledTextItem,
-  StyledImgLink,
-  StyledButton,
-} from './Blog.styled';
+import { StyledButton } from './Blog.styled';
+import { Link } from 'react-router-dom';
 
 import Label from '../../../components/Label/Label';
 
@@ -33,7 +16,7 @@ const events = [
     date: { day: '08', month: 'September' },
     text: 'We are dedicated in creating added value for our customers by implementing modern technology in our work. ',
     textList: ['Urgent transport solutions', 'Reliable & experienced staffs'],
-    href: '#',
+    href: '/blog',
   },
   {
     title: 'How technology can help redraw the supply chain map',
@@ -41,7 +24,7 @@ const events = [
     date: { day: '12', month: 'September' },
     text: 'We are dedicated in creating added value for our customers by implementing modern technology in our work. ',
     textList: ['Urgent transport solutions', 'Reliable & experienced staffs'],
-    href: '#',
+    href: '/blog',
   },
 
   {
@@ -50,45 +33,75 @@ const events = [
     date: { day: '25', month: 'September' },
     text: 'We are dedicated in creating added value for our customers by implementing modern technology in our work. ',
     textList: ['Urgent transport solutions', 'Reliable & experienced staffs'],
-    href: '#',
+    href: '/blog',
   },
 ];
 
 export default function Blog() {
   const elements = events.map(
     ({ title, img, date: { day, month }, text, textList, href }) => (
-      <StyledEventItem key={title}>
-        <StyledImgContainer bg={img}>
-          <StyledImgLink to={href}>Read More</StyledImgLink>
-        </StyledImgContainer>
-        <StyledDateContainer>
-          <StyledIconContainer>
+      <li
+        className="flex p-7 first:border-t-[1px] first:border-t-solid first:border-t-[#d6d6d6]"
+        style={{ borderBottom: '1px solid #d6d6d6' }}
+        key={title}
+      >
+        <div
+          className="w-[453px] h-[308px] flex justify-center items-center [&:hover>a]:opacity-100"
+          style={{ backgroundImage: `url(${img})` }}
+        >
+          <Link
+            className=" text-white font-krab text-base font-semibold py-[100px] px-[146px] bg-[#1c1f35e8]/90 opacity-0 transition-opacity duration-300 ease-in-out"
+            to={href}
+          >
+            Read More
+          </Link>
+        </div>
+        <div className="flex flex-col items-center px-5 border-r-[1px] border-r-solid border-r-[#d6d6d6]">
+          <div className="w-[51px] h-[56px]">
             <img src={calendar} alt="calendar" />
-          </StyledIconContainer>
-          <StyledDayText>{day}</StyledDayText>
-          <StyledMonthText>{month}</StyledMonthText>
-        </StyledDateContainer>
-        <StyledTextContainer>
-          <StyledTitle to={href}>{title}</StyledTitle>
-          <StyledText>{text}</StyledText>
-          <StyledTextListContainer>
+          </div>
+          <p className=" text-4xl font-semibold font-rubik text-heading-color">
+            {day}
+          </p>
+          <p className=" text-base font-krab font-medium text-paragraph-color">
+            {month}
+          </p>
+        </div>
+        <div className="w-[335px] flex flex-col gap-4 pl-7">
+          <Link
+            className=" font-rubik text-2xl font-normal text-heading-color transition-colors hover:text-primary-yelow duration-200 ease-in-out"
+            to={href}
+          >
+            {title}
+          </Link>
+          <p className=" font-krab font-medium text-paragraph-color text-base">
+            {text}
+          </p>
+          <ul className="flex flex-col pl-7 ">
             {textList.map(item => (
-              <StyledTextItem key={item}>{item}</StyledTextItem>
+              <li
+                className=" font-krab font-medium text-base list-disc text-paragraph-color"
+                key={item}
+              >
+                {item}
+              </li>
             ))}
-          </StyledTextListContainer>
-        </StyledTextContainer>
-      </StyledEventItem>
+          </ul>
+        </div>
+      </li>
     )
   );
 
   return (
-    <StyledSection>
-      <StyledContainer>
+    <section className=" py-24">
+      <div className="containerStyled flex flex-col items-center">
         <Label text="Our Blog" />
-        <StyledTtileSection>Our Latest News</StyledTtileSection>
-        <StyledEventsList>{elements}</StyledEventsList>
+        <h3 className=" font-rubik text-4xl font-semibold text-heading-color pt-3 pb-10">
+          Our Latest News
+        </h3>
+        <ul className="flex flex-col ">{elements}</ul>
         <StyledButton to="/blog">More Blog</StyledButton>
-      </StyledContainer>
-    </StyledSection>
+      </div>
+    </section>
   );
 }
--- src/components/HomePage/Blog/Blog.styled.jsx
+++ src/components/HomePage/Blog/Blog.styled.jsx
@@ -1,161 +1,160 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-export const StyledSection = styled.section`
-  background-color: #fff;
-  padding-top: 98px;
-  padding-bottom: 98px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
+// export const StyledSection = styled.section`
+//   background-color: #fff;
+//   padding-top: 98px;
+//   padding-bottom: 98px;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+// `;
 
-export const StyledContainer = styled.div`
-  width: 1230px;
-  padding-left: 15px;
-  padding-right: 15px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
+// export const StyledContainer = styled.div`
+//   width: 1230px;
+//   padding-left: 15px;
+//   padding-right: 15px;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+// `;
 
-export const StyledTtileSection = styled.h3`
-  color: #1c1f35;
-  font-family: Rubik;
-  font-size: 35px;
-  font-style: normal;
-  font-weight: 600;
-  line-height: normal;
+// export const StyledTtileSection = styled.h3`
+//   color: #1c1f35;
+//   font-family: Rubik;
+//   font-size: 35px;
+//   font-style: normal;
+//   font-weight: 600;
+//   line-height: normal;
 
-  padding-top: 13px;
-  padding-bottom: 39px;
-`;
+//   padding-top: 13px;
+//   padding-bottom: 39px;
+// `;
 
-export const StyledEventsList = styled.ul`
-  display: flex;
-  flex-direction: column;
-`;
+// export const StyledEventsList = styled.ul`
+//   display: flex;
+//   flex-direction: column;
+// `;
 
-export const StyledEventItem = styled.li`
-  display: flex;
-  padding-top: 29px;
-  padding-bottom: 29px;
-  border-bottom: 1px solid #d6d6d6;
+// export const StyledEventItem = styled.li`
+//   display: flex;
+//   padding-top: 29px;
+//   padding-bottom: 29px;
+//   border-bottom: 1px solid #d6d6d6;
 
-  &:first-child {
-    border-top: 1px solid #d6d6d6;
-  }
-`;
+//   &:first-child {
+//     border-top: 1px solid #d6d6d6;
+//   }
+// `;
 
-export const StyledImgContainer = styled.div`
-  width: 453px;
-  height: 308px;
-  background-image: url(${({ bg }) => bg});
+// export const StyledImgContainer = styled.div`
+//   width: 453px;
+//   height: 308px;
+//   background-image: url(${({ bg }) => bg});
 
-  display: flex;
-  justify-content: center;
-  align-items: center;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
 
-  &:hover a {
-    opacity: 1;
-  }
-`;
+//   &:hover a {
+//     opacity: 1;
+//   }
+// `;
 
-export const StyledImgLink = styled(Link)`
-  padding: 100px 146px;
-  background-color: rgba(28, 31, 53, 0.91);
-  text-decoration: none;
+// export const StyledImgLink = styled(Link)`
+//   padding: 100px 146px;
+//   background-color: #1c1f35e8;
+//   text-decoration: none;
 
-  color: #fff;
-  font-family: Krub;
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 600;
-  line-height: 135.023%;
-  opacity: 0;
-  transition: opacity 0.35s ease-in-out;
-`;
+//   color: #fff;
+//   font-family: Krub;
+//   font-size: 16px;
+//   font-style: normal;
+//   font-weight: 600;
+//   line-height: 135.023%;
+//   opacity: 0;
+//   transition: opacity 0.35s ease-in-out;
+// `;
 
-export const StyledDateContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding-left: 20px;
-  padding-right: 20px;
-  border-right: 1px solid #d6d6d6;
-`;
+// export const StyledDateContainer = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   padding-left: 20px;
+//   padding-right: 20px;
+//   border-right: 1px solid #d6d6d6;
+// `;
 
-export const StyledIconContainer = styled.div`
-  width: 51px;
-  height: 56px;
-`;
+// export const StyledIconContainer = styled.div`
+//   width: 51px;
+//   height: 56px;
+// `;
 
-export const StyledDayText = styled.p`
-  color: #1c1f35;
-  font-family: Rubik;
-  font-size: 40px;
-  font-style: normal;
-  font-weight: 600;
-  line-height: normal;
-`;
+// export const StyledDayText = styled.p`
+//   color: #1c1f35;
+//   font-family: Rubik;
+//   font-size: 40px;
+//   font-style: normal;
+//   font-weight: 600;
+//   line-height: normal;
+// `;
 
-export const StyledMonthText = styled.p`
-  color: #666c89;
-  font-family: Krub;
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 500;
-  line-height: 151.523%;
-`;
+// export const StyledMonthText = styled.p`
+//   color: #666c89;
+//   font-family: Krub;
+//   font-size: 16px;
+//   font-style: normal;
+//   font-weight: 500;
+//   line-height: 151.523%;
+// `;
 
-export const StyledTextContainer = styled.div`
-  width: 335px;
-  display: flex;
-  flex-direction: column;
-  gap: 18px;
-  padding-left: 29px;
-`;
+// export const StyledTextContainer = styled.div`
+//   width: 335px;
+//   display: flex;
+//   flex-direction: column;
+//   gap: 18px;
+//   padding-left: 29px;
+// `;
 
-export const StyledTitle = styled(Link)`
-  color: #1c1f35;
-  font-family: Rubik;
-  font-size: 25px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: normal;
-  text-decoration: none;
+// export const StyledTitle = styled(Link)`
+//   color: #1c1f35;
+//   font-family: Rubik;
+//   font-size: 25px;
+//   font-style: normal;
+//   font-weight: 400;
+//   line-height: normal;
+//   text-decoration: none;
 
-  transition: color 0.25s ease-in-out;
+//   transition: color 0.25s ease-in-out;
 
-  &:hover {
-    color: #ffbe34;
-  }
-`;
+//   &:hover {
+//     color: #ffbe34;
+//   }
+// `;
 
-export const StyledText = styled.p`
-  color: #666c89;
-  font-family: Krub;
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 500;
-  line-height: 151.523%;
-`;
+// export const StyledText = styled.p`
+//   color: #666c89;
+//   font-family: Krub;
+//   font-size: 16px;
+//   font-style: normal;
+//   font-weight: 500;
+//   line-height: 151.523%;
+// `;
 
-export const StyledTextListContainer = styled.ul`
-  display: flex;
-  flex-direction: column;
-  list-style: inside;
-`;
+// export const StyledTextListContainer = styled.ul`
+//   display: flex;
+//   flex-direction: column;
+//   list-style: inside;
+// `;
 
-export const StyledTextItem = styled.li`
-  color: #1c1f35;
-  font-family: Krub;
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 500;
-  line-height: 166.523%;
-  list-style: inside;
-`;
+// export const StyledTextItem = styled.li`
+//   color: #1c1f35;
+//   font-family: Krub;
+//   font-size: 16px;
+//   font-style: normal;
+//   font-weight: 500;
+//   line-height: 166.523%;
+// `;
 
 export const StyledButton = styled(Link)`
   width: 194px;
