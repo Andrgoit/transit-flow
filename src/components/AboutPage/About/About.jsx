dd986bb8cb5589ede5be56b89e65e2c9459bd03d
Andrgoit <stakgrom@gmail.com>
2023-09-27 20:17:20 +0300

    refactoring about section => tailwindcss

--- src/components/AboutPage/About/About.jsx
+++ src/components/AboutPage/About/About.jsx
@@ -1,53 +1,73 @@
-import {
-  AboutContainer,
-  StyledContainer,
-  StyledTextArea,
-  StyledTitle,
-  StyledTextContainer,
-  StyledText,
-  StyledImgArea,
-  StyledContentImage,
-  StyledButtonContainer,
-  StyledButton,
-} from './About.styled';
+import { Link } from 'react-router-dom';
 
-import Buttom from './Button/Buttom';
-
+import LinkButton from '../../LinkButton/LinkButton';
 import Label from '../../../components/Label/Label';
+
+import bg from '../../../img/AboutPage/About/big.jpg';
+import image from '../../../img/AboutPage/About/small.jpg';
 
 export default function About() {
   return (
-    <AboutContainer>
-      <StyledContainer>
-        <StyledImgArea>
-          <StyledContentImage />
-        </StyledImgArea>
+    <section className=" py-[100px] bg-white flex justify-center">
+      <div className="flex gap-[100px]">
+        <div
+          className=" relative w-[726px] h-[486px]"
+          style={{
+            background: `url(${bg})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+          }}
+        >
+          <div
+            className=" absolute w-[264px] h-[258px] -bottom-[45px] -right-[40px]"
+            style={{
+              background: `url(${image})`,
+              borderTop: '25px solid #fff',
+              borderLeft: '25px solid #fff ',
+            }}
+          />
+        </div>
 
-        <StyledTextArea>
+        <div className=" w-[472px] flex flex-col items-start">
           <Label text="About Us" />
-          <StyledTitle>Our Company Overview</StyledTitle>
-          <StyledTextContainer>
-            <StyledText>
-              Leverage agile frameworks to provide a robust synopsis for
-              strategy foster collaborative thinking to further the overall
-              value proposition.
-            </StyledText>
-            <StyledButtonContainer>
-              <StyledButton to="#">Our Approch</StyledButton>
-              <StyledButton to="#">Our Approch</StyledButton>
-              <StyledButton to="#">Our Approch</StyledButton>
-            </StyledButtonContainer>
-            <StyledText>
-              Sed ut perspiciatis, unde omnis iste natus error sit voluptatem
-              accusantium doloremque laudantium, totam rem aperiam eaque ipsa,
-              quae ab illo inventore veritatis et quasi architecto beatae vitae
-              dicta sunt, explicabo. Nemo enim ipsam voluptatem quia
-              voluptassit.
-            </StyledText>
-          </StyledTextContainer>
-          <Buttom />
-        </StyledTextArea>
-      </StyledContainer>
-    </AboutContainer>
+          <h3 className=" font-rubik font-semibold text-4xl text-heading-color my-3">
+            Our Company Overview
+          </h3>
+
+          <p className=" font-krab font-medium text-base text-paragraph-color mb-5">
+            Leverage agile frameworks to provide a robust synopsis for strategy
+            foster collaborative thinking to further the overall value
+            proposition.
+          </p>
+          <div className=" flex justify-center gap-[14px] mb-7">
+            <Link
+              className=" w-[194px] py-4 px-[38px] font-krab font-semibold text-lg text-heading-color cursor-pointer transition-colors duration-300 bg-secondary-gray hover:text-white hover:bg-primary-yelow"
+              to="#"
+            >
+              Our Approch
+            </Link>
+            <Link
+              className=" w-[194px] py-4 px-[38px] font-krab font-semibold text-lg text-heading-color cursor-pointer transition-colors duration-300 bg-secondary-gray hover:text-white hover:bg-primary-yelow"
+              to="#"
+            >
+              Our Approch
+            </Link>
+            <Link
+              className=" w-[194px] py-4 px-[38px] font-krab font-semibold text-lg text-heading-color cursor-pointer transition-colors duration-300 bg-secondary-gray hover:text-white hover:bg-primary-yelow"
+              to="#"
+            >
+              Our Approch
+            </Link>
+          </div>
+          <p className=" font-krab font-medium text-base text-paragraph-color mb-9">
+            Sed ut perspiciatis, unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam eaque ipsa,
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt, explicabo. Nemo enim ipsam voluptatem quia voluptassit.
+          </p>
+          <LinkButton to="/blog">Learn More</LinkButton>
+        </div>
+      </div>
+    </section>
   );
 }
